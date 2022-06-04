31f4f51f625f84f5203425096673e85b6feae761
yohan <yohanrr86@gmail.com>
2022-06-04 16:14:53 -0500

    Adding Exercise

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona p = new Persona();
+        p.setEdad(23);
+        System.out.println(p.getEdad());
+        p.setNombre("Yohan");
+        System.out.println(p.getNombre());
+        p.setTelefono(1232198);
+        System.out.println(p.getTelefono());
+    }
+}
